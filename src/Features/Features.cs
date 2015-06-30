ae57b4c47be6ca231b4e2d3c04690c7545042680
Alex Kamsteeg <alex@atlex.nl>
2015-06-30 19:07:37 +0200

    Renamed the Features project to Features.Base

--- src/Features/Features.cs
+++ src/Features/Features.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Features
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class Features
     {
     }
